e4f0cf15b8de38250c931cdc587731baaca57452
krovskiy <168024863+krovskiy@users.noreply.github.com>
2025-03-04 20:16:31 +0200

    second commit

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def main():
     })
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=5000)
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/meow': 'http://localhost:5000',
+    }
+  },
 });
